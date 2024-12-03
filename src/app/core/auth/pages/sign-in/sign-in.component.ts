3a7d422bfdc8c96af0c3b8647dd1f5df72b52ec5
athenexfolk <folkadpocket99@gmail.com>
2024-12-03 08:56:09 +0700

    added reactive form to sign in page

--- src/app/core/auth/pages/sign-in/sign-in.component.ts
+++ src/app/core/auth/pages/sign-in/sign-in.component.ts
@@ -1,26 +1,45 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../auth.service';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'v-sign-in',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
   readonly auth = inject(AuthService);
+  private readonly fb = inject(FormBuilder);
   private readonly router = inject(Router);
 
-  username = '';
-  password = '';
+  form = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  get username() {
+    return this.form.get('username');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
 
   isAuthFailed = false;
 
   signIn() {
     this.auth
-      .signIn({ username: this.username, password: this.password })
+      .signIn({
+        username: this.username?.value!,
+        password: this.password?.value!,
+      })
       .subscribe({
         next: () => {
           this.router.navigate(['/']);
