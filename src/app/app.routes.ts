d3a80065f50f5648df874fd904a685de47eb08c9
athenexfolk <folkadpocket99@gmail.com>
2024-12-03 11:34:10 +0700

    added content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
 import { LandingComponent } from './features/landing/pages/landing/landing.component';
 import { ResourceComponent } from './features/resource/pages/resource/resource.component';
 import { SignInComponent } from './core/auth/pages/sign-in/sign-in.component';
 import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
+import { ContentComponent } from './features/resource/pages/content/content.component';
+import { inject } from '@angular/core';
+import { ContentService } from './core/services/content.service';
+import { catchError, of } from 'rxjs';
 
 export const routes: Routes = [
   {
@@ -16,6 +20,19 @@ export const routes: Routes = [
       {
         path: 'resource',
         component: ResourceComponent,
+      },
+      {
+        path: 'contents/:id',
+        component: ContentComponent,
+        resolve: {
+          lesson: (route: ActivatedRouteSnapshot) => {
+            const contentService = inject(ContentService);
+
+            return contentService
+              .getLessonById(route.paramMap.get('id')!)
+              .pipe(catchError(() => of(null)));
+          },
+        },
       },
       {
         path: 'manage-resource',
